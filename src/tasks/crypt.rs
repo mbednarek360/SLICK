92617767091ce572b282f51f22f8be277410c3c6
Michael Bednarek <mbednarek360@protonmail.com>
2019-04-05 21:58:40 -0400

    Began implementation of non trivial permutation

--- src/tasks/crypt.rs
+++ src/tasks/crypt.rs
@@ -3,33 +3,28 @@
 fn vec_pad(k: u64, v: &Vec<u8>, e: bool) -> Vec<u8> {
     let mut out = v.clone();
     let l = v.len() as u64;
-    if e {
-        out.push(255);
-        for i in 0..((k - l) - 1) {
-            out.push(0);
+    if k > l {
+        if e {
+            out.push(255);
+            for i in 0..((k - l) - 1) {
+                out.push(0);
+            }
         }
-    }
-    else {
-        while out[out.len() - 1] != 255 {
+        else {
+            while out[out.len() - 1] != 255 {
+                out.pop();
+            }
             out.pop();
         }
-        out.pop();
     }
     return out;
 }
 
 
 // ----------------------------------------------------------------
-// find vector constant
-fn vec_const(k: u128, l: u64) -> u64 {
-    return ((k / l as u128) + (k % l as u128)) as u64;
-}
-
-
-// ----------------------------------------------------------------
 // calculate position to find value for current byte
-fn vec_position(i: u64, c: u64, l: u64) -> u64 {
-    return (c - i) % l;
+fn vec_position(k: u128, s: u64) -> usize {
+    return (k % s as u128) as usize;
 }
 
 
@@ -49,25 +44,20 @@ fn vec_shift(b: u8, n: u64, e: bool) -> u8 {
 // ----------------------------------------------------------------
 // primary byte vector crypt function 
 pub fn vec_crypt(k: u128, l: u64, v: &Vec<u8>, e: bool) -> Vec<u8> {
+    let mut x = k;
     let mut a: Vec<u8>;
     if e {
         a = vec_pad(l, v, e);
     } else {
-        a = v.clone()
+        a = v.clone();
     }
     let mut f = Vec::new();
-    let c = vec_const(k, l);
-    for i in 0..l {
-        let p = vec_position(i, c, l);
-        let s;
-        if e {
-            s = p + 1;
-        }
-        else {
-            s = i + 1;
-        }
-        let b = vec_shift(a[p as usize], s, e);
-        f.push(b);
+    for p in 0..l {
+       let s = l - p;
+       let b = vec_position(x, s);
+       f.push(a[b]);
+       a.remove(b);
+       x /= s as u128;
     }
     if e {
         return f;
