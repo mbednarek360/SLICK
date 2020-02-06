f663dc91c024d97035be4631ca8003c8624e2edb
Michael Bednarek <mbednarek360@protonmail.com>
2020-02-06 18:51:07 -0500

    Removed deprecated crates

--- build_web.sh
+++ build_web.sh
@@ -1,5 +1,8 @@
 rm Cargo.toml
+rm -r web/core
+cp -r core web/core
 ln -s Cargo_web.toml Cargo.toml
 rm -r web/pkg
 wasm-pack build --target web --release
 mv pkg web
+rm Cargo.toml
--- cli/core/key.rs
+++ cli/core/key.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
-use rustc_serialize::base64::{self, ToBase64, FromBase64};
-use rustc_serialize::hex::{ToHex, FromHex};
+use base64;
+use hex;
 
 
 // ----------------------------------------------------------------
@@ -28,14 +28,14 @@ pub fn max_key(c: usize) -> u128 {
 pub fn gen_key(l: usize, r: u16) -> String {
     let k: u128 = rand::thread_rng().gen_range(1, max_key(l));
     let s = format!("{:0>4x}{:0>16X}{:0>32X}", r, l, k);
-    return s.from_hex().unwrap().as_slice().to_base64(base64::STANDARD)
+    return base64::encode(&hex::decode(s).unwrap());
 }
 
 
 // ----------------------------------------------------------------
 // parses key string to length and seed
 pub fn parse_key(k: &String) -> (u16, usize, u128) {
-    let h = k.from_base64().unwrap().as_slice().to_hex();
+    let h = hex::encode(base64::decode(k).unwrap());
     let r = u16::from_str_radix(&h[0..4], 16).unwrap();
     let l = usize::from_str_radix(&h[4..20], 16).unwrap();
     let s = u128::from_str_radix(&h[20..52], 16).unwrap();
